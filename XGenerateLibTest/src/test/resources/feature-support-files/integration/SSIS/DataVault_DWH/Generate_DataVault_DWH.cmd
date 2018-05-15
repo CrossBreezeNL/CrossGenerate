dc93bf8e925691940191a0303f9859be7735b8d1
Willem <willem@x-breeze.com>
2018-05-15 15:05:54 +0200

    created separate config for staging SSIS in DWH integration test

--- XGenerateLibTest/src/test/resources/feature-support-files/integration/SSIS/DataVault_DWH/Generate_DataVault_DWH.cmd
+++ XGenerateLibTest/src/test/resources/feature-support-files/integration/SSIS/DataVault_DWH/Generate_DataVault_DWH.cmd
@@ -20,7 +20,7 @@ echo Generating Staging DDL and ETL...
     -mtc source_model.xml::ssis\DataVault_Staging_System_name\Load_DataVault_Staging_System_name.dtsx::DataVault_Staging_master.xml ^
     -mtc source_model.xml::ssis\DataVault_Staging_System_name\DataVault_Staging_System_name.dtproj::DataVault_Staging_DTProj.xml ^
     -mtc reference_model.xml::sql\DataVault_DWH\tables\Staging_Table_System_name.sql::DataVault_Staging_SQL.xml ^
-    -mtc reference_model.xml::ssis\DataVault_Staging_System_name\Load_System_name_Entity_name.dtsx::DataVault_DWH_SSIS.xml ^
+    -mtc reference_model.xml::ssis\DataVault_Staging_System_name\Load_System_name_Entity_name.dtsx::DataVault_Staging_SSIS.xml ^
     -mtc reference_model.xml::ssis\DataVault_Staging_System_name\Load_DataVault_Staging_System_name.dtsx::DataVault_Staging_master.xml ^
     -mtc reference_model.xml::ssis\DataVault_Staging_System_name\DataVault_Staging_System_name.dtproj::DataVault_Staging_DTProj.xml ^
     -fld "%OutputDir%\%LogFile%"
