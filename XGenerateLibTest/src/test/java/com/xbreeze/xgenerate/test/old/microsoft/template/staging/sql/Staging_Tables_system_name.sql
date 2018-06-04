329635c8681072cb89e4782465671cd23a5b6dd4
Harmen Wessels <harmen@x-breeze.com>
2018-06-05 00:54:54 +0200

    Removed old tests.
    Related Work Items: #281

--- XGenerateLibTest/src/test/java/com/xbreeze/xgenerate/test/old/microsoft/template/staging/sql/Staging_Tables_system_name.sql
+++
@@ -1,7 +0,0 @@
--- @XGenTextSection(name="CreateTable" literalOnLastLine="GO")
-CREATE TABLE [system_name].[entity_name] (
-  -- @XGenTextSection(name="TableColumn")
-  [attribute_name]           attribute_fulldatatype        NULL,
-  [StageDateTime]            datetime2(2)              NOT NULL  
-);
-GO
