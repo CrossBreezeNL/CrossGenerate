500fad3076ed767d98ef43df5ed5e9d84f987c7e
Willem <willem@x-breeze.com>
2021-04-30 21:34:10 +0200

    changed mkdocs to use docker and updated yaml

--- Documentation/mkdocs_build.ps1
+++ Documentation/mkdocs_build.ps1
@@ -2,4 +2,4 @@
 
 REM The following command will build the HTML version of the documentation based on the current docs files.
 REM The output will be written in the 'site' folder.
-mkdocs build --clean
+docker run --rm -it -v ${PWD}:/docs squidfunk/mkdocs-material build
---
+++ Documentation/mkdocs_serve.ps1
@@ -0,0 +1,2 @@
+start chrome http://localhost:8001
+docker run --rm -it -p 8001:8000 -v ${PWD}:/docs squidfunk/mkdocs-material
