5d3c5f23f2ecf29d1173f51a2d6f76dbd71e8a69
Harmen Wessels <harmen@X-Breeze.com>
2021-08-13 18:05:25 +0200

    Fixed error in powershell script for mkdocs. Updated htaccess for latest folder.

--- Documentation/mkdocs_build.ps1
+++ Documentation/mkdocs_build.ps1
@@ -1,5 +1,3 @@
-@echo off
-
-REM The following command will build the HTML version of the documentation based on the current docs files.
-REM The output will be written in the 'site' folder.
+# The following command will build the HTML version of the documentation based on the current docs files.
+# The output will be written in the 'site' folder.
 docker run --rm -it -v ${PWD}:/docs squidfunk/mkdocs-material build
